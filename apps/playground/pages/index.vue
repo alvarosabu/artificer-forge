70dfccf1e08c62e5b8ddb00c71365981ecebb61d
alvarosabu <alvaro.saburido@gmail.com>
2026-07-07 11:48:02 +0200

    chore(playground): link modular character demo on the main page

--- apps/playground/pages/index.vue
+++ apps/playground/pages/index.vue
@@ -21,6 +21,7 @@ const categories: DemoCategory[] = [
     icon: 'i-heroicons-user',
     links: [
       { label: 'Basic', to: '/character/basic' },
+      { label: 'Modular (Tav)', to: '/character/modular' },
       { label: 'Large', to: '/character/large' },
       { label: 'Party', to: '/character/party' },
       { label: 'Character Portrait', to: '/character/portraits' },
